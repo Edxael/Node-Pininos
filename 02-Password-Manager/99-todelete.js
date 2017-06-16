8fc4dd0c40cbb5bb537ce387e0a1a25155800fb1
Edxael <zerphyr.r@gmail.com>
2017-06-15 19:53:28 -0600

    Update on Project

---
+++ 02-Password-Manager/99-todelete.js
@@ -0,0 +1,6 @@
+console.log(" Hello ");
+console.log(" My name is Edxael ");
+console.log(" I study web development ");
+console.log(" I live in SLC ");
+console.log(" I run in the mornings ");
+console.log(" and eat YUNK ");
---
+++ 02-Password-Manager/app.js
@@ -0,0 +1,5 @@
+console.log("script: app.js");
+console.log("Starting Edxael's Passwor Manager");
+
+var storage = require('node-persist'); // this is how you add 3rd party modules in your App.
+storage.initSync(); // <-- gets the computer ready to Writing & Storing variables.
